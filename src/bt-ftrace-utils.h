siemens/bt2-ftrace-to-ctf

commit f36593f9d479b737938e2e33ed60ff2116f9a337

refactor: make conditional event handling reusable
As a preparation to reuse the simple checks if the event belongs to a
specific event system (and alike), we move them to a utility header.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/bt-ftrace-utils.h
@@ -1,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (C) 2026 Siemens
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef _BT_FTRACE_UTILS_H
+#define _BT_FTRACE_UTILS_H
+
+#include <event-parse.h>
+
+/**
+ * Returns 1 if the event has the given prefix, 0 otherwise
+ */
+static inline int event_has_prefix(const char *prefix,
+								   const struct tep_event *event)
+{
+	if (strncmp(prefix, event->name, strlen(prefix)) == 0) {
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Returns 1 if the event system matches name (string comparison)
+ */
+static inline int event_system_is(const char *name,
+								  const struct tep_event *event)
+{
+	if (strncmp(name, event->system, strlen(name)) == 0) {
+		return true;
+	}
+	return false;
+}
+
+#endif

meson.build
@@ -70,7 +70,8 @@ bt2_ftrace_plugin = shared_library('babeltrace-plugin-ftrace',
     'src/bt-ftrace-source.c',
     'src/bt-ftrace-source.h',
     'src/bt-ftrace-tracemeta.c',
-    'src/bt-ftrace-tracemeta.h'
+    'src/bt-ftrace-tracemeta.h',
+    'src/bt-ftrace-utils.h',
     ),
   c_args: ['-Wno-unused-parameter'],
   gnu_symbol_visibility: bt2_plugin_sym_vis,

src/bt-ftrace-lttng-events.c
@@ -4,6 +4,7 @@
  */
 
 #include "bt-ftrace-lttng-events.h"
+#include "bt-ftrace-utils.h"
 
 #include <string.h>
 
@@ -30,22 +31,6 @@ static const char *lttng_field_name_replace_pid_by_tid(const char *field_name)
 		}
 	}
 	return outbuf;
-}
-
-static int event_has_prefix(const char *prefix, const struct tep_event *event)
-{
-	if (strncmp(prefix, event->name, strlen(prefix)) == 0) {
-		return true;
-	}
-	return false;
-}
-
-static int event_system_is(const char *name, const struct tep_event *event)
-{
-	if (strncmp(name, event->system, strlen(name)) == 0) {
-		return true;
-	}
-	return false;
 }
 
 const char *event_prefix_name(const char *prefix, const struct tep_event *event)

commit 6ef92e5eac60631e927b578d22f1156a2f9fb4c5

fix(source): match event system by full name
Previously a prefix match was applied, but we better resort to a full
match. There are currently no subsystems where this makes a difference,
but this might change in the future.

Fixes: f36593f ("refactor: make conditional event handling ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/bt-ftrace-utils.h
@@ -27,7 +27,7 @@ static inline int event_has_prefix(const char *prefix,
 static inline int event_system_is(const char *name,
 								  const struct tep_event *event)
 {
-	if (strncmp(name, event->system, strlen(name)) == 0) {
+	if (strcmp(name, event->system) == 0) {
 		return true;
 	}
 	return false;