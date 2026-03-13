siemens/bt2-ftrace-to-ctf

commit b6406edc77e20d7f8e8986a6bdbb3a901b4fc81a

refactor(plugin): check for bt2 symbols instead of version
We currently use version checks to see if the babeltrace library offers
some symbols. We switch that over to directly checking for the symbols,
as this is a more reliable way of doing it.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/bt-ftrace-source.c
@@ -234,7 +234,7 @@ static void create_metadata_and_stream(bt_self_component *self_component,
 								  ftrace_in->clock_offset_ns % NS_PER_S);
 		bt_clock_class_origin_is_unix_epoch(clock_class);
 	} else {
-#if BT2_VERSION_MINOR >= 1
+#if HAS_BT2_CLOCK_UNKNOWN
 		bt_clock_class_set_origin_unknown(clock_class);
 #endif
 	}
@@ -244,7 +244,7 @@ static void create_metadata_and_stream(bt_self_component *self_component,
 			uuid_parse(ftrace_in->clock_uid, clock_uuid);
 			bt_clock_class_set_uuid(clock_class, clock_uuid);
 		} else {
-#if BT2_VERSION_MINOR >= 1
+#if HAS_BT2_CLOCK_UID
 			bt_clock_class_set_uid(clock_class, ftrace_in->clock_uid);
 #endif
 		}
@@ -297,7 +297,7 @@ static void create_metadata_and_stream(bt_self_component *self_component,
 	/* Create a default trace from (instance of `trace_class`) */
 	bt_trace *trace = bt_trace_create(trace_class);
 	sprintf(NAME_BUF, "%llu", tracecmd_get_traceid(ftrace_in->tc_input));
-#if BT2_VERSION_MINOR >= 1
+#if HAS_BT2_HAS_TRACE_UID
 	if (mip_version >= 2) {
 		bt_trace_set_uid(trace, NAME_BUF);
 	}

meson.build
@@ -25,8 +25,20 @@ bt2_dep_version = babeltrace2_dep.version().split('.')
 tc_has_reverse_it = cc.has_function('tracecmd_iterate_events_reverse',
                                     dependencies: tracecmd_dep,
                                     required: false)
+bt2_has_clock_uid = cc.has_function('bt_clock_class_set_uid',
+                                    dependencies: babeltrace2_dep,
+                                    required: false)
+bt2_has_clock_unknown = cc.has_function('bt_clock_class_set_origin_unknown',
+                                        dependencies: babeltrace2_dep,
+                                        required: false)
+bt2_has_trace_uid = cc.has_function('bt_trace_set_uid',
+                                    dependencies: babeltrace2_dep,
+                                    required: false)
+
 add_project_arguments(
-  '-DBT2_VERSION_MINOR=' + bt2_dep_version[1],
+  '-DHAS_BT2_CLOCK_UID=' + bt2_has_clock_uid.to_string(),
+  '-DHAS_BT2_CLOCK_UNKNOWN=' + bt2_has_clock_unknown.to_string(),
+  '-DHAS_BT2_TRACE_UID=' + bt2_has_trace_uid.to_string(),
   '-DHAS_TRACECMD_REVERSE_ITERATION=' + tc_has_reverse_it.to_string(),
   language : 'c'
 )

src/bt-ftrace-tracemeta.c
@@ -124,7 +124,7 @@ static void emit_metadata_json(struct tracemeta_out *cm_out,
 		json_builder_set_member_name(builder, "uuid");
 		json_builder_add_string_value(builder, uuid_buf);
 	} else if (cm_out->mip_version > 0) {
-#if BT2_VERSION_MINOR >= 1
+#if HAS_BT2_TRACE_UID
 		const char *trace_uid = bt_trace_get_uid(trace);
 		json_builder_set_member_name(builder, "uid");
 		json_builder_add_string_value(builder, trace_uid);
@@ -181,7 +181,7 @@ static void emit_metadata_json(struct tracemeta_out *cm_out,
 		json_builder_set_member_name(builder, "uuid");
 		json_builder_add_string_value(builder, uuid_buf);
 	} else if (cm_out->mip_version > 0) {
-#if BT2_VERSION_MINOR >= 1
+#if HAS_BT2_CLOCK_UID
 		const char *clock_uid = bt_clock_class_get_uid(clock_cls);
 		json_builder_set_member_name(builder, "uid");
 		json_builder_add_string_value(builder, clock_uid);

commit 73527679ac8a9929415818b37ba09ef664c3d8f7

fix(source): use correct macro name to check for bt2 trace uid symbol
Fixes: b6406ed ("refactor(plugin): check for bt2 symbols instead ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/bt-ftrace-source.c
@@ -437,7 +437,7 @@ static void create_metadata_and_trace(bt_self_component *self_component,
 	/* Create a default trace from (instance of `trace_class`) */
 	bt_trace *trace = bt_trace_create(trace_class);
 	sprintf(NAME_BUF, "%llu", tracecmd_get_traceid(tc_main));
-#if HAS_BT2_HAS_TRACE_UID
+#if HAS_BT2_TRACE_UID
 	if (mip_version >= 2) {
 		bt_trace_set_uid(trace, NAME_BUF);
 	}