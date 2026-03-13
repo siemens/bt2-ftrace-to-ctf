siemens/bt2-ftrace-to-ctf

commit 7519ed288c260a689d192b46eeeb33b4da87375f

feat: pass trace metadata from US trace to kernel trace
The sink.ctf.fs sink has logic that detects if a processed trace is an
LTTng trace. If it is, the output directory is layed out as done by
LTTng. To support this, we need to pass metadata from the LTTng US trace
into the ftrace source. This is implemented in a two staged approach:
Metadata we can read from the ftrace itself is just added. Metadata
(like the trace-name) that is only available in the LTTng US trace is
copied over.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/bt-ftrace-source.c
@@ -27,6 +27,9 @@
 #include "bt-ftrace-lttng-events.h"
 #include "bt-ftrace-logging.h"
 #include "bt-ftrace-source.h"
+#if WITH_TRACE_CMD_PRIVATE_SYMBOLS
+#include "trace-cmd-private.h"
+#endif
 
 #include <babeltrace2/babeltrace.h>
 #include <event-parse.h>
@@ -64,7 +67,9 @@ struct ftrace_in {
 
 	/* tracer and trace metadata */
 	char *trace_name;
+	char *trace_sysname;
 	char *trace_hostname;
+	char *trace_kernel_release;
 	char *trace_creation_datetime;
 	int tracer_version_major;
 	int tracer_version_minor;
@@ -84,6 +89,23 @@ struct ftrace_in {
 	struct port_in **port_data;
 	unsigned int nb_port_data;
 };
+
+/*
+ * Parse options of the trace.dat file
+ */
+static void parse_tracedat_opts(struct ftrace_in *ftrace_in)
+{
+#if WITH_TRACE_CMD_PRIVATE_SYMBOLS
+	const char *uname = tracecmd_get_uname(ftrace_in->tc_input);
+	char *uname_copy = strdup(uname);
+	ftrace_in->trace_sysname = strdup(strtok(uname_copy, " "));
+	ftrace_in->trace_hostname = strdup(strtok(NULL, " "));
+	ftrace_in->trace_kernel_release = strdup(strtok(NULL, " "));
+	free(uname_copy);
+#else
+	ftrace_in->trace_sysname = strdup("Linux");
+#endif
+}
 
 /*
  * Creates an event class within `stream_class` from a ftrace event.
@@ -277,14 +299,22 @@ static void create_metadata_and_stream(bt_self_component *self_component,
 		bt_trace_set_uid(trace, NAME_BUF);
 	}
 #endif
-	/* TODO: make trace name configurable */
-	bt_trace_set_name(trace, "global-kernel-session");
+	if (ftrace_in->trace_name) {
+		bt_trace_set_name(trace, ftrace_in->trace_name);
+	}
 	bt_trace_set_environment_entry_string(trace, "domain", "kernel");
-	bt_trace_set_environment_entry_string(trace, "sysname", "Linux");
+	bt_trace_set_environment_entry_string(trace, "sysname",
+										  ftrace_in->trace_sysname);
+	if (ftrace_in->trace_kernel_release) {
+		bt_trace_set_environment_entry_string(trace, "kernel_release",
+											  ftrace_in->trace_kernel_release);
+	}
 	bt_trace_set_environment_entry_string(trace, "trace_buffering_scheme",
 										  "global");
-	bt_trace_set_environment_entry_string(trace, "trace_name",
-										  "global-kernel-session");
+	if (ftrace_in->trace_name) {
+		bt_trace_set_environment_entry_string(trace, "trace_name",
+											  ftrace_in->trace_name);
+	}
 	/* The CTF sink dispatches based on this value. Fake it */
 	bt_trace_set_environment_entry_string(
 		trace, "tracer_name",
@@ -378,7 +408,6 @@ ftrace_in_initialize(bt_self_component_source *self_component_source,
 	if (trace_name_val) {
 		ftrace_in->trace_name = strdup(bt_value_string_get(trace_name_val));
 	}
-	/* TODO: trace hostname from trace.dat */
 	const bt_value *trace_date_val = bt_value_map_borrow_entry_value_const(
 		params, "trace-creation-datetime");
 	if (trace_date_val) {
@@ -394,6 +423,8 @@ ftrace_in_initialize(bt_self_component_source *self_component_source,
 	ftrace_in->tep = tracecmd_get_tep(ftrace_in->tc_input);
 	const int ncpus = tep_get_cpus(ftrace_in->tep);
 	BT_FTRACE_LOG_INFO(ftrace_in->log_level, "the trace has %d CPUs", ncpus);
+
+	parse_tracedat_opts(ftrace_in);
 
 	bt_self_component *self_component =
 		bt_self_component_source_as_self_component(self_component_source);
@@ -449,6 +480,8 @@ void ftrace_in_finalize(bt_self_component_source *self_component_source)
 	free(ftrace_in->clock_uid);
 	free(ftrace_in->trace_name);
 	free(ftrace_in->trace_hostname);
+	free(ftrace_in->trace_sysname);
+	free(ftrace_in->trace_kernel_release);
 	free(ftrace_in->trace_creation_datetime);
 	free(ftrace_in);
 }

src/bt-ftrace-tracemeta.c
@@ -5,8 +5,10 @@
  * The tracemeta sink emits per-stream clock metadata in json-lines format that
  * can be used to sync the clocks of multiple traces. The format is as following:
  * {
+ *   trace: { uid: <str> | uuid: <str> },
  *   stream: { id: <int>, name: <str>},
- *   clock: { offset_s: <int>, offset_c: <int>, frequency: <int> }
+ *   clock: { offset_s: <int>, offset_c: <int>, frequency: <int>, uid: str | uuid: str },
+ *   env: { { name: value }, ... }
  * }
  * The sink component uses the following initialization parameters:
  * 
@@ -97,20 +99,57 @@ static void emit_metadata_json(struct tracemeta_out *cm_out,
 	int64_t offset_sec;
 	uint64_t offset_cycles, freq;
 	bt_uuid clock_uuid = NULL;
+	bt_uuid trace_uuid = NULL;
+	char uuid_buf[UUID_STR_LEN];
 
 	const bt_stream *stream =
 		bt_message_stream_beginning_borrow_stream_const(message);
+	const bt_trace *trace = bt_stream_borrow_trace_const(stream);
 	const bt_clock_class *clock_cls =
 		bt_message_stream_beginning_borrow_stream_class_default_clock_class_const(
 			message);
 	bt_clock_class_get_offset(clock_cls, &offset_sec, &offset_cycles);
 	freq = bt_clock_class_get_frequency(clock_cls);
-	if (cm_out->mip_version == 0) {
-		clock_uuid = bt_clock_class_get_uuid(clock_cls);
-	}
 
 	JsonBuilder *builder = json_builder_new();
 	json_builder_begin_object(builder);
+
+	/* trace object */
+	json_builder_set_member_name(builder, "trace");
+	json_builder_begin_object(builder);
+
+	if (cm_out->mip_version == 0) {
+		trace_uuid = bt_trace_get_uuid(trace);
+		uuid_unparse(trace_uuid, uuid_buf);
+		json_builder_set_member_name(builder, "uuid");
+		json_builder_add_string_value(builder, uuid_buf);
+	} else if (cm_out->mip_version > 0) {
+#if BT2_VERSION_MINOR >= 1
+		const char *trace_uid = bt_trace_get_uid(trace);
+		json_builder_set_member_name(builder, "uid");
+		json_builder_add_string_value(builder, trace_uid);
+#endif
+	}
+	json_builder_end_object(builder);
+
+	/* environment object */
+	json_builder_set_member_name(builder, "env");
+	json_builder_begin_object(builder);
+	const uint64_t env_nentries = bt_trace_get_environment_entry_count(trace);
+	for (uint64_t i = 0; i < env_nentries; i++) {
+		const char *name;
+		const bt_value *value;
+		bt_trace_borrow_environment_entry_by_index_const(trace, i, &name,
+														 &value);
+		json_builder_set_member_name(builder, name);
+		if (bt_value_is_string(value)) {
+			json_builder_add_string_value(builder, bt_value_string_get(value));
+		} else {
+			json_builder_add_int_value(builder,
+									   bt_value_integer_signed_get(value));
+		}
+	}
+	json_builder_end_object(builder);
 
 	/* stream object */
 	json_builder_set_member_name(builder, "stream");
@@ -136,8 +175,8 @@ static void emit_metadata_json(struct tracemeta_out *cm_out,
 	json_builder_set_member_name(builder, "frequency");
 	json_builder_add_int_value(builder, (gint64)freq);
 
-	if (clock_uuid) {
-		char uuid_buf[UUID_STR_LEN];
+	if (cm_out->mip_version == 0) {
+		clock_uuid = bt_clock_class_get_uuid(clock_cls);
 		uuid_unparse(clock_uuid, uuid_buf);
 		json_builder_set_member_name(builder, "uuid");
 		json_builder_add_string_value(builder, uuid_buf);

src/ftrace-to-ctf.c
@@ -23,10 +23,22 @@ typedef struct {
 	uint64_t clock_offset;
 	char *clock_uid;
 	int mip;
+	char *trace_datetime;
+	char *trace_name;
 	char *trace_path;
 	char *lttng_path;
 	char *out_dir;
+	int loglevel;
 } prog_opts;
+
+typedef struct {
+	char *clock_uid;
+	int64_t clock_offset_s;
+	uint64_t clock_offset_c;
+	uint64_t clock_freq;
+	char *trace_datetime;
+	char *trace_name;
+} trace_metadata;
 
 static void print_usage(char *prog_name)
 {
@@ -36,9 +48,12 @@ static void print_usage(char *prog_name)
 		"\n"
 		"Options:\n"
 		"  -c, --ctf-version <v> CTF version to use (default: 1.8)\n"
+		"  -d, --trace-dt <name> ISO‑8601 timestamp of the trace\n"
 		"  -l, --lttng           Convert well-known events to LTTng representation (default: off)\n"
+		"  -n, --trace-name <name> Name of the trace (session)\n"
 		"  -o, --clock-offset <offset> Trace clock offset in ns to world clock\n"
 		"  -u, --clock-uid <(u)uid> Trace clock uuid or uid, depending on MIP version\n"
+		"  -v, --verbose         Increase logging level (repeatable)\n"
 		"  -h, --help            Show this help message and exit\n",
 		basename(prog_name));
 }
@@ -66,14 +81,9 @@ static bool check_ctf_version(const char *ctf_version, int *mip_version)
 int parse_args(int argc, char *argv[], prog_opts *opts)
 {
 	/* Initialise defaults */
+	memset(opts, 0, sizeof(*opts));
 	opts->ctf_version = "1.8";
-	opts->mip = 0;
-	opts->lttng = false;
-	opts->clock_offset = 0;
-	opts->clock_uid = NULL;
-	opts->trace_path = NULL;
-	opts->lttng_path = NULL;
-	opts->out_dir = NULL;
+	opts->loglevel = BT_LOGGING_LEVEL_WARNING;
 
 	// clang-format off
 	static const struct option long_opts[] = {
@@ -81,6 +91,9 @@ int parse_args(int argc, char *argv[], prog_opts *opts)
 		{ "clock-offset", required_argument, 0, 'o'},
 		{ "clock-uid",   required_argument, 0, 'u' },
 		{ "lttng",       no_argument,       0, 'l' },
+		{ "trace-dt",    required_argument, 0, 'd' },
+		{ "trace-name",  required_argument, 0, 'n' },
+		{ "verbose",     no_argument,       0, 'v' },
 		{ "help",        no_argument,       0, 'h' },
 		{ 0,             0,                 0,  0  }
 	};
@@ -89,20 +102,29 @@ int parse_args(int argc, char *argv[], prog_opts *opts)
 	int opt;
 	int opt_index = 0;
 
-	while ((opt = getopt_long(argc, argv, "c:lho:u:", long_opts, &opt_index)) !=
-		   -1) {
+	while ((opt = getopt_long(argc, argv, "c:d:ln:o:u:vh", long_opts,
+							  &opt_index)) != -1) {
 		switch (opt) {
 		case 'c':
 			opts->ctf_version = optarg;
 			break;
+		case 'd':
+			opts->trace_datetime = strdup(optarg);
+			break;
 		case 'l':
 			opts->lttng = true;
+			break;
+		case 'n':
+			opts->trace_name = strdup(optarg);
 			break;
 		case 'o':
 			opts->clock_offset = strtoull(optarg, NULL, 10);
 			break;
 		case 'u':
 			opts->clock_uid = strdup(optarg);
+			break;
+		case 'v':
+			opts->loglevel = opts->loglevel > 0 ? opts->loglevel - 1 : 0;
 			break;
 		case 'h': /* fall‑through */
 		case '?': /* unknown option */
@@ -168,8 +190,7 @@ const bt_plugin *load_plugin_by_name(char *name)
 }
 
 /* Parse the clock definitions emitted by sink.ftrace.tracemeta */
-static int parse_clock_meta(const char *buffer, int64_t *offset_s,
-							uint64_t *offset_c, uint64_t *freq, char **uid)
+static int parse_trace_meta(const char *buffer, trace_metadata *trace_meta)
 {
 	JsonParser *parser = json_parser_new();
 	GError *error = NULL;
@@ -202,19 +223,31 @@ static int parse_clock_meta(const char *buffer, int64_t *offset_s,
 		return -1;
 	}
 
-	*offset_s = (guint64)json_object_get_int_member(clock_o, "offset_s");
-	*offset_c = (guint64)json_object_get_int_member(clock_o, "offset_c");
-	*freq = (guint64)json_object_get_int_member(clock_o, "frequency");
+	trace_meta->clock_offset_s =
+		(guint64)json_object_get_int_member(clock_o, "offset_s");
+	trace_meta->clock_offset_c =
+		(guint64)json_object_get_int_member(clock_o, "offset_c");
+	trace_meta->clock_freq =
+		(guint64)json_object_get_int_member(clock_o, "frequency");
 	const char *uid_str =
 		json_object_get_string_member_with_default(clock_o, "uid", "");
 	if (*uid_str) {
-		*uid = strdup(uid_str);
+		trace_meta->clock_uid = strdup(uid_str);
 	} else {
 		/* Either uid or uuid must be set, so we can be sure to have it */
 		const char *uuid_str = json_object_get_string_member(clock_o, "uuid");
-		*uid = strdup(uuid_str);
+		trace_meta->clock_uid = strdup(uuid_str);
 	}
 
+	JsonObject *env_o = json_object_get_object_member(root_o, "env");
+	if (json_object_has_member(env_o, "trace_name")) {
+		trace_meta->trace_name =
+			strdup(json_object_get_string_member(env_o, "trace_name"));
+	}
+	if (json_object_has_member(env_o, "trace_creation_datetime")) {
+		trace_meta->trace_datetime = strdup(
+			json_object_get_string_member(env_o, "trace_creation_datetime"));
+	}
 	g_object_unref(parser);
 	return 0;
 }
@@ -231,18 +264,19 @@ static int parse_clock_meta(const char *buffer, int64_t *offset_s,
  * Note: This parser needs to be kept in sync with the generator in
  * sink.ftrace.tracemeta.
  */
-static int get_clock_offset_from_lttng_trace(const bt_plugin *ftrace_plugin,
-											 const bt_plugin *ctf_plugin,
-											 const bt_plugin *utils_plugin,
-											 prog_opts *opts)
+static int get_metadata_from_lttng_trace(const bt_plugin *ftrace_plugin,
+										 const bt_plugin *ctf_plugin,
+										 const bt_plugin *utils_plugin,
+										 prog_opts *opts)
 {
 	const bt_component_source *source;
 	const bt_component_filter *filter;
 	const bt_component_sink *sink;
+	trace_metadata trace_meta;
 	int out_fds[2];
-	int64_t offset_s;
-	uint64_t offset_c, freq;
-	char *clock_uid = NULL;
+	int status = 0;
+
+	memset(&trace_meta, 0, sizeof(trace_meta));
 
 	const bt_component_class_source *source_cls =
 		bt_plugin_borrow_source_component_class_by_name_const(ctf_plugin, "fs");
@@ -266,17 +300,17 @@ static int get_clock_offset_from_lttng_trace(const bt_plugin *ftrace_plugin,
 	bt_value_map_insert_empty_array_entry(comp_params, "inputs", &inputs);
 	bt_value_array_append_string_element(inputs, opts->lttng_path);
 	bt_graph_add_source_component(graph, source_cls, "lttng", comp_params,
-								  BT_LOGGING_LEVEL_WARNING, &source);
+								  opts->loglevel, &source);
 	bt_value_put_ref(comp_params);
 
 	bt_graph_add_filter_component(graph, filter_cls, "muxer", NULL,
-								  BT_LOGGING_LEVEL_WARNING, &filter);
+								  opts->loglevel, &filter);
 
 	comp_params = bt_value_map_create();
 	bt_value_map_insert_signed_integer_entry(comp_params, "outfd", out_fds[1]);
 
 	bt_graph_add_sink_component(graph, sink_cls, "tracemeta", comp_params,
-								BT_LOGGING_LEVEL_WARNING, &sink);
+								opts->loglevel, &sink);
 	bt_value_put_ref(comp_params);
 
 	/* plumbing */
@@ -312,14 +346,17 @@ static int get_clock_offset_from_lttng_trace(const bt_plugin *ftrace_plugin,
 	size_t len = 0;
 	ssize_t nb = getline(&line, &len, fp);
 	line[nb] = '\0';
-	int ok = parse_clock_meta(line, &offset_s, &offset_c, &freq, &clock_uid);
-	if (ok == 0) {
-		opts->clock_offset = offset_s * freq + offset_c;
-		opts->clock_uid = clock_uid;
+	status = parse_trace_meta(line, &trace_meta);
+	if (status == 0) {
+		opts->clock_offset = trace_meta.clock_offset_s * trace_meta.clock_freq +
+							 trace_meta.clock_offset_c;
+		opts->clock_uid = trace_meta.clock_uid;
+		opts->trace_datetime = trace_meta.trace_datetime;
+		opts->trace_name = trace_meta.trace_name;
 	}
 	free(line);
 	fclose(fp);
-	return ok;
+	return status;
 }
 
 int main(int argc, char **argv)
@@ -338,8 +375,6 @@ int main(int argc, char **argv)
 	printf("Options parsed:\n");
 	printf("  lttng :       %s\n", opts.lttng ? "yes" : "no");
 	printf("  ctf-version : %s\n", opts.ctf_version);
-	if (opts.clock_offset)
-		printf("  clock-offset: %lu\n", opts.clock_offset);
 	printf("  trace   :     %s\n", opts.trace_path);
 	printf("  lttng-trace:  %s\n",
 		   opts.lttng_path ? opts.lttng_path : "not provided");
@@ -357,13 +392,18 @@ int main(int argc, char **argv)
 	if (!ctf_plugin)
 		return -1;
 
-	/* only get clock offset if not explicitly provided */
-	if (opts.lttng_path && !opts.clock_offset) {
-		get_clock_offset_from_lttng_trace(ftrace_plugin, ctf_plugin,
-										  utils_plugin, &opts);
-		printf("  clock-offset: %lu (from CTF trace)\n", opts.clock_offset);
-		printf("  clock-uid:    %s (from CTF trace)\n", opts.clock_uid);
+	if (opts.lttng_path) {
+		get_metadata_from_lttng_trace(ftrace_plugin, ctf_plugin, utils_plugin,
+									  &opts);
 	}
+	printf("  clock-offset: %lu\n", opts.clock_offset);
+	printf("  clock-uid:    %s\n",
+		   opts.clock_uid ? opts.clock_uid : "not provided");
+	printf("  trace-date:   %s\n",
+		   opts.trace_datetime ? opts.trace_datetime : "not provided");
+	printf("  trace-name:   %s\n",
+		   opts.trace_name ? opts.trace_name : "not provided");
+
 	/* TODO: check and handle errors */
 
 	const bt_component_class_source *source_cls =
@@ -396,10 +436,20 @@ int main(int argc, char **argv)
 		bt_value_map_insert_string_entry(source_params, "clock-uid",
 										 opts.clock_uid);
 	}
+	if (opts.trace_name) {
+		bt_value_map_insert_string_entry(source_params, "trace-name",
+										 opts.trace_name);
+	}
+	if (opts.trace_datetime) {
+		bt_value_map_insert_string_entry(
+			source_params, "trace-creation-datetime", opts.trace_datetime);
+	}
 	bt_graph_add_source_component(graph, source_cls, "ftrace", source_params,
-								  BT_LOGGING_LEVEL_WARNING, &source);
+								  opts.loglevel, &source);
 	bt_value_put_ref(source_params);
 	free(opts.clock_uid);
+	free(opts.trace_datetime);
+	free(opts.trace_name);
 
 	/* optional lttng trace input */
 	if (opts.lttng_path) {
@@ -407,20 +457,20 @@ int main(int argc, char **argv)
 		bt_value_map_insert_empty_array_entry(source_params, "inputs", &inputs);
 		bt_value_array_append_string_element(inputs, opts.lttng_path);
 		bt_graph_add_source_component(graph, source_lttng_cls, "lttng",
-									  source_params, BT_LOGGING_LEVEL_WARNING,
+									  source_params, opts.loglevel,
 									  &source_lttng);
 		bt_value_put_ref(source_params);
 	}
 
 	bt_graph_add_filter_component(graph, filter_cls, "muxer", NULL,
-								  BT_LOGGING_LEVEL_WARNING, &filter);
+								  opts.loglevel, &filter);
 
 	bt_value *sink_params = bt_value_map_create();
 	bt_value_map_insert_string_entry(sink_params, "path", opts.out_dir);
 	bt_value_map_insert_string_entry(sink_params, "ctf-version",
 									 opts.ctf_version);
 	bt_graph_add_sink_component(graph, sink_cls, "fs", sink_params,
-								BT_LOGGING_LEVEL_WARNING, &sink);
+								opts.loglevel, &sink);
 	bt_value_put_ref(sink_params);
 
 	const uint64_t nb_ft_ports =

commit 87993196c222257b48386305c373ba6be597ebf3

fix(source): safely parse tracedat opts
We change the parsing in a way that avoids UB on unknown input data.

Fixes: 7519ed2 ("feat: pass trace metadata from US trace to ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

src/bt-ftrace-source.c
@@ -123,9 +123,15 @@ static void parse_tracedat_opts(struct ftrace_in *ftrace_in)
 #if WITH_TRACE_CMD_PRIVATE_SYMBOLS
 	const char *uname = tracecmd_get_uname(ftrace_in->tc_buffers[0].tc_input);
 	char *uname_copy = strdup(uname);
-	ftrace_in->trace_sysname = strdup(strtok(uname_copy, " "));
-	ftrace_in->trace_hostname = strdup(strtok(NULL, " "));
-	ftrace_in->trace_kernel_release = strdup(strtok(NULL, " "));
+	char *saveptr = NULL;
+	char *tok;
+
+	tok = strtok_r(uname_copy, " ", &saveptr);
+	ftrace_in->trace_sysname = tok ? strdup(tok) : strdup("Linux");
+	tok = tok ? strtok_r(NULL, " ", &saveptr) : NULL;
+	ftrace_in->trace_hostname = tok ? strdup(tok) : NULL;
+	tok = tok ? strtok_r(NULL, " ", &saveptr) : NULL;
+	ftrace_in->trace_kernel_release = tok ? strdup(tok) : NULL;
 	free(uname_copy);
 #else
 	ftrace_in->trace_sysname = strdup("Linux");